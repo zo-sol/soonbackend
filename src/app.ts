6761dc4174b14d18f0ab275238a728be6091ad25
sumin <dwckey5356@gmail.com>
2025-03-25 17:42:27 +0900

    docker

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import setupRoutes from './routes';
-import { config } from 'dotenv';
-config();
 import { configs } from './configs';
 
 const app = express();
@@ -9,5 +7,5 @@ app.use(express.json());
 setupRoutes(app);
 
 app.listen(configs.port, () => {
-    console.log(`API server running on port ${configs.port}`);
+    console.log(`API server running on port ${process.env.PORT}`);
 });
