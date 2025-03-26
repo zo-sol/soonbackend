8ff7acebd4dc1fcd6ff65a480af7643ce20057c8
sumin <dwckey5356@gmail.com>
2025-03-26 16:33:11 +0900

    allow all for testing

--- src/routes/cors.routes.ts
+++ src/routes/cors.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import cors from 'cors';
 
+// const allowedOrigins = [
+//     "https://iq6900.com",
+//     "https://elizacodein.com",
+//     "https://eliza-codein.pages.dev",
+//     "https://testbrowserforiq.web.app"
+// ];
 const allowedOrigins = [
-    "https://iq6900.com",
-    "https://elizacodein.com",
-    "https://eliza-codein.pages.dev",
-    "https://testbrowserforiq.web.app"
+    "*" // allow all for testing
 ];
-
 export default (router: Router): void => {
     const r = Router();
     r.use(
