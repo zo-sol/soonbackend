f098e3b067adb5a7432ba107957baea0bca5d506
sumin <dwckey5356@gmail.com>
2025-03-29 22:20:14 +0900

    unlock for test

--- src/routes/cors.routes.ts
+++ src/routes/cors.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import cors from 'cors';
-
+//
+// const allowedOrigins = [
+// "https://iq6900.com",
+// "https://elizacodein.com",
+// "https://eliza-codein.pages.dev",
+// "https://testbrowserforiq.web.app",
+// "http://127.0.0.1:3000"
+// ];
 const allowedOrigins = [
-"https://iq6900.com",
-"https://elizacodein.com",
-"https://eliza-codein.pages.dev",
-"https://testbrowserforiq.web.app",
-"http://127.0.0.1:3000"
+   "*"
 ];
-
 export default (router: Router): void => {
     const r = Router();
     r.use(
