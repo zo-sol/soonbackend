b8fef0cc696eb59b48151066972efce2928018f3
sumin <dwckey5356@gmail.com>
2025-03-29 23:03:18 +0900

    cors

--- src/routes/cors.routes.ts
+++ src/routes/cors.routes.ts
@@ -1,29 +1,30 @@
-import { Router } from 'express';
+import {Router} from 'express';
 import cors from 'cors';
-//
-// const allowedOrigins = [
-// "https://iq6900.com",
-// "https://elizacodein.com",
-// "https://eliza-codein.pages.dev",
-// "https://testbrowserforiq.web.app",
-// "http://127.0.0.1:3000"
-// ];
+
 const allowedOrigins = [
-   "*"
+    "https://iq6900.com",
+    "https://elizacodein.com",
+    "https://eliza-codein.pages.dev",
+    "https://testbrowserforiq.web.app",
+    "https://127.0.0.1:3000",
+    "https://localhost:3000"
 ];
+// const allowedOrigins = [
+//    "*"
+// ];
 export default (router: Router): void => {
     const r = Router();
     r.use(
         cors({
             origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
+                if (!origin || allowedOrigins.includes(origin)) {
+                    callback(null, true);
+                } else {
+                    callback(new Error("Not allowed by CORS"));
+                }
             },
         })
     );
-    
+
     router.use('/', r);
 }
