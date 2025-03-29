7bb1533970762164325b8935a987e15a19e387ed
sumin <dwckey5356@gmail.com>
2025-03-29 23:34:32 +0900

    cors

--- src/routes/cors.routes.ts
+++ src/routes/cors.routes.ts
@@ -1,5 +1,15 @@
 import {Router} from 'express';
 import cors from 'cors';
+
+export default (router: Router): void => {
+    const r = Router();
+    r.use(cors({
+        origin: '*', // 모든 origin 허용
+    }));
+
+    router.use('/', r);
+}
+
 //
 // const allowedOrigins = [
 //     "https://iq6900.com",
@@ -8,36 +18,8 @@ import cors from 'cors';
 //     "https://testbrowserforiq.web.app"
 //
 // ];
-const allowedOrigins = [
-    "*"
-];
-
-export default (router: Router): void => {
-    const r = Router();
-    r.use(
-        cors({
-            origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-                if (!origin || allowedOrigins.includes(origin)) {
-                    callback(null, true);
-                } else {
-                    callback(new Error("Not allowed by CORS"));
-                }
-            },
-        })
-    );
-
-    router.use('/', r);
-}
-// import {Router} from 'express';
-// import cors from 'cors';
-//
 // const allowedOrigins = [
-//     "https://iq6900.com",
-//     "https://elizacodein.com",
-//     "https://eliza-codein.pages.dev",
-//     "https://testbrowserforiq.web.app",
-//     "https://127.0.0.1:3000",
-//     "https://localhost:3000"
+//     "*"
 // ];
 //
 // export default (router: Router): void => {
