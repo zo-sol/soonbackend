6022b6b5db3d436bc0375a43ba3b57b5e1224507
sumin <dwckey5356@gmail.com>
2025-03-29 23:18:29 +0900

    cors

--- src/routes/cors.routes.ts
+++ src/routes/cors.routes.ts
@@ -1,14 +1,33 @@
+import {Router} from 'express';
 import cors from 'cors';
-import express from 'express';
-// 모든 출처 허용 + 모든 메서드 허용 + 모든 헤더 허용
-const app = express();
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST"], // 모든 메서드 허용
-    allowedHeaders: ["*"], // 모든 헤더 허용
-}));
+//
+// const allowedOrigins = [
+//     "https://iq6900.com",
+//     "https://elizacodein.com",
+//     "https://eliza-codein.pages.dev",
+//     "https://testbrowserforiq.web.app"
+//
+// ];
+const allowedOrigins = [
+    "*"
+];
 
-app.options("*", cors());
+export default (router: Router): void => {
+    const r = Router();
+    r.use(
+        cors({
+            origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+                if (!origin || allowedOrigins.includes(origin)) {
+                    callback(null, true);
+                } else {
+                    callback(new Error("Not allowed by CORS"));
+                }
+            },
+        })
+    );
+
+    router.use('/', r);
+}
 // import {Router} from 'express';
 // import cors from 'cors';
 //
