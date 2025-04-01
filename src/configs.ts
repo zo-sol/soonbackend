020c5913657c3dc014277f81ba8fb3e71d1cac26
sumin <dwckey5356@gmail.com>
2025-04-01 14:00:44 +0900

    make this for testnet

--- src/configs.ts
+++ src/configs.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const configs = {
-    network: "https://rpc.svmbnbtestnet.soo.network/rpc",
+    network: "https://rpc.testnet.soo.network/rpc",
     dbSeed: process.env.DB_SEED || '',
     dataSeed: process.env.DATA_SEED || '',
     port: process.env.MYPORT || 8080,
@@ -10,10 +10,10 @@ export const configs = {
 
 export const constants = {
     expectedReceiver: new PublicKey("4w2UTBBaBfC16fuqk7nCr58FTx8mwqK3cimeT5uvy9Xh"),
-    // programId: new PublicKey("EY4HPYAFb2UbJxDBPvkbzp6YErMdWXN32RXKj2z8iCUe") //testnet
+    programId: new PublicKey("EY4HPYAFb2UbJxDBPvkbzp6YErMdWXN32RXKj2z8iCUe") //testnet
    // programId: new PublicKey("HyPbmCVDcEu1vNHzPCghb9skQEbam2LBQEk9BFZQphHQ")//mainnet
 
-    programId: new PublicKey("FTnvQFnhZNPKHmm9vEhxz25HS3FR8NiWv56zm7t8VUgx")
+   // programId: new PublicKey("FTnvQFnhZNPKHmm9vEhxz25HS3FR8NiWv56zm7t8VUgx")//bnb test
 
 
 
