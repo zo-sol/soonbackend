68d09368f59f90bed6c95b59fdb98ec47f0e552a
sumin <dwckey5356@gmail.com>
2025-04-01 16:02:57 +0900

    change testnet address

--- src/configs.ts
+++ src/configs.ts
@@ -10,10 +10,11 @@ export const configs = {
 
 export const constants = {
     expectedReceiver: new PublicKey("9q1FEzFWr39bwmYEofu7DTX2qUAJK6oiytavxiqqPVmA"),
-    programId: new PublicKey("EY4HPYAFb2UbJxDBPvkbzp6YErMdWXN32RXKj2z8iCUe") //testnet
+    programId: new PublicKey("Bwe1NNEGgxPrTzb1FXGcdfUTmMKfinfJ5fkb48hEUfcw") //testnet
    // programId: new PublicKey("HyPbmCVDcEu1vNHzPCghb9skQEbam2LBQEk9BFZQphHQ")//mainnet
 
-   // programId: new PublicKey("FTnvQFnhZNPKHmm9vEhxz25HS3FR8NiWv56zm7t8VUgx")//bnb test
+   // programId: new PublicKey("6ryCoEffUvi484etNsgMMQSPWNUod9YAJqTcKEPHnoPh")//bnb test
+
 
 
 
