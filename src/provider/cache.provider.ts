08f9d17c015e83e003b2fe2d982c0cfd86d14cc5
sumin <dwckey5356@gmail.com>
2025-04-02 14:04:59 +0900

    isit bad

--- src/provider/cache.provider.ts
+++ src/provider/cache.provider.ts
@@ -27,16 +27,14 @@ export const getTxListFromDb = async (
         },
     });
 
-    let database: any;
+    await client.close();
     await client.connect();
-    database = client.db("TransactionLists");
     console.log('Connected to MongoDB');
-    const collection = database.collection(`${targetAddress}/${category}`);
-
     const pageSize = 100;
     let filter: any = { block_time: { $lt: lastBlockTime } };  // blockTime 기준 필터링
 
-    const result = await collection
+    const result = await client.db("TransactionLists")
+        .collection(`${targetAddress}/${category}`)
         .find(filter)
         .sort({block_time: -1})  // 최신 blockTime 기준 내림차순 정렬
         .limit(pageSize)
