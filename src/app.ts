a11d4deab05fcc8d06f19393d6063fe91fa54115
sumin <dwckey5356@gmail.com>
2025-05-15 21:41:12 +0900

    add limit

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import setupRoutes from './routes';
 import { configs } from './configs';
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 setupRoutes(app);
 
 const port = configs.port || process.env.MYPORT || 8080;
