0a7c9adf25168e826124da07a590913db41d45a4
sumin <dwckey5356@gmail.com>
2025-05-17 23:09:13 -0400

    unlimit

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import setupRoutes from './routes';
 import { configs } from './configs';
 
 const app = express();
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '10mb' }));
 setupRoutes(app);
 
 const port = configs.port || process.env.MYPORT || 8080;
